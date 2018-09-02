b2d62562032121dc60ea8f6e57b271a739021381
unknown <cp.mcpike@gmail.com>
2018-09-01 20:05:17 -0700

    finished except the repeating crystal functions.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,28 +1,45 @@
 var randNum, userTotal;
 var wins = 0, losses = 0;
 
+// Create 4 crystals that each have a random value between 1-12 (values not displayed)
+var blueCryst = (Math.floor(Math.random() * (12 - 1) + 1));
+var yelCryst = (Math.floor(Math.random() * (12 - 1) + 1));
+var greenCryst = (Math.floor(Math.random() * (12 - 1) + 1));
+var redCryst = (Math.floor(Math.random() * (12 - 1) + 1));
+
 var checkGameState = function() {
     // User wins if they hit the random number without going over
     if (userTotal === randNum) {
-        alert("You Win!");
+        alert('You win!');
         wins++;
         $("#wins").text(wins);
+        setTimeout("reset()", 500);
     }
 
     // User loses if they go over the random number
     if (userTotal > randNum) {
-        alert("You lose!");
+        alert('Bummer, bruh. Try again!');
         losses++;
         $("#losses").text(losses);
+        setTimeout("reset()", 500);
     }
+}
+
+var reset = function() {
+    randNum = Math.floor(Math.random() * (120 - 19) + 19);
+    $("#random-number").text(randNum);
+    userTotal = 0;
+    $("#user-number").text(userTotal);
+    blueCryst = (Math.floor(Math.random() * (12 - 1) + 1));
+    yelCryst = (Math.floor(Math.random() * (12 - 1) + 1));
+    greenCryst = (Math.floor(Math.random() * (12 - 1) + 1));
+    redCryst = (Math.floor(Math.random() * (12 - 1) + 1));
 }
 
 $(document).ready(function() {
     
     // Create random number between 19-120 to be displayed to user
     randNum = Math.floor(Math.random() * (120 - 19) + 19);
-
-    console.log("randNum is: " + randNum);
 
     $("#random-number").text(randNum);
 
@@ -31,38 +48,27 @@ $(document).ready(function() {
 
     $("#user-number").text(userTotal);
 
-    // Create 4 crystals that each have a random value between 1-12 (values not displayed)
-    var blueCryst = (Math.floor(Math.random() * (12 - 1) + 1));
-    var yelCryst = (Math.floor(Math.random() * (12 - 1) + 1));
-    var greenCryst = (Math.floor(Math.random() * (12 - 1) + 1));
-    var redCryst = (Math.floor(Math.random() * (12 - 1) + 1));
-
     $("#bluecrystal").on("click", function() { 
         userTotal += blueCryst;
         $("#user-number").text(userTotal);
-        console.log("blue: " + blueCryst);
         checkGameState();
         });
 
     $("#yellowcrystal").on("click", function() { 
         userTotal += yelCryst;
         $("#user-number").text(userTotal);
-        console.log("yellow: " + yelCryst);
         checkGameState();
         });
 
     $("#greencrystal").on("click", function() { 
         userTotal += greenCryst;
         $("#user-number").text(userTotal);
-        console.log("green " + greenCryst);
         checkGameState();
         });
 
     $("#redcrystal").on("click", function() { 
         userTotal += redCryst;
         $("#user-number").text(userTotal);
-        console.log("red " + redCryst);
-        console.log("user total: " +userTotal);
         checkGameState();
         });
   
